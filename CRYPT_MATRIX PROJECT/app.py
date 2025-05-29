667ad2cbffa813914d2f7e4b95339d0bacfcac0f
M4rci4l <marempillo@my.cspc.edu.ph>
2025-05-29 09:50:08 +0800

    Update app.py

--- CRYPT_MATRIX PROJECT/app.py
+++ CRYPT_MATRIX PROJECT/app.py
@@ -9,8 +9,6 @@ import base64
 import secrets
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///MarcialRemps09$CryptMatrix_2.db'  # or PostgreSQL/MySQL URL
-db = SQLAlchemy(app)
 
 # ---------- ROUTES ----------
 @app.route('/')
